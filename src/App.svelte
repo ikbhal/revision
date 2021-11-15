625ec108b6373f53cf92c84199d2fd2e1b0074fd
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-15 15:43:58 +0530

    add few more scenarios

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,22 @@
 <main>
 <h1>Revision</h1>
 <p>Revise english, people talk, ui ux, coding, lang, framework, cooking</p>
+<p>
+    If yoou read any thing, you will remember for some days(duration/period).
+    After that you will forget.
+    If you revise it you will remember it.
+    ---
+    This apply to human relationship as well.
+    If you dont connect with friend for long time. The friend ship is not
+    ---
+    spent time with family
+    ---
+    spritual practice: namaaz, quran, jikr, jamaat
+    ---
+    health 
+        walking, gym, 
+        sports: running, 
+</p>
 </main>
 
 <style>
